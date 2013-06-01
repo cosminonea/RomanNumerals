27915226c704e34a06a5cdaf8b54d93215c0e000
cosmin.onea <cosmin.onea@gmail.com>
2013-06-01 21:29:09 +0100

    converting 1-9 to roman numerals

---
+++ src/RomanNumerals/RomanNumeralsConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace RomanNumerals.UnitTests
+{
+    using System.Text;
+
+    public class RomanNumeralsConverter
+    {
+        private static readonly int[] Values = { 9, 5, 4, 1 };
+        private static readonly string[] Symbols = { "IX", "V", "IV", "I" };
+
+        public string Convert(int number)
+        {
+            var result = new StringBuilder();
+
+            var remaining = number;
+
+            for (int i = 0; i < Values.Length; i++)
+            {
+                remaining = Convert(remaining, Values[i], Symbols[i], result);                
+            }
+
+            return result.ToString();
+        }
+
+        private static int Convert(int remaining, int value, string symbol, StringBuilder result)
+        {
+            while (remaining >= value)
+            {
+                result.Append(symbol);
+                remaining -= value;
+            }
+
+            return remaining;
+        }
+    }
+}
---
+++ test/RomanNumerals.UnitTests/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace RomanNumerals.UnitTests
+{
+    using NUnit.Framework;
+
+    public class SomeTest
+    {
+        [TestCase(1, Result = "I")]
+        [TestCase(2, Result = "II")]
+        [TestCase(3, Result = "III")]
+        [TestCase(4, Result = "IV")]
+        [TestCase(5, Result = "V")]
+        [TestCase(6, Result = "VI")]
+        [TestCase(7, Result = "VII")]
+        [TestCase(8, Result = "VIII")]
+        [TestCase(9, Result = "IX")]
+        public string ShouldConvertOne(int number)
+        {
+            var converter = new RomanNumeralsConverter();
+
+            return converter.Convert(number);
+        }
+    }
+}
